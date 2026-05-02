82472b52789a7bd365c495d30ea5b32e6761736f
seoyoung-dev <seoyoung.dev@gmail.com>
2026-05-02 15:28:06 +0900

    Revert "Add slide 3 visual asset page at /?slides"
    This reverts commit a6787eaf8bb15b662e9a02d526356f355e34aa37.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import GameScreen from './pages/GameScreen'
-import SlideAssets from './pages/SlideAssets'
 
 export default function App() {
-  if (new URLSearchParams(window.location.search).get('slides') !== null) {
-    return <SlideAssets />
-  }
   return <GameScreen />
 }
